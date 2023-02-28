19c3b8c39eb627c5140cb21e03b62e13978779f0
Vuk Stojic IT51-2020 <vukstojic@yahoo.com>
2023-02-28 08:52:12 +0100

    Inicajli Commit

---
+++ VezbeProjekat/src/test/java/rva/VezbeProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezbeProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
